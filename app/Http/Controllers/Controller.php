f72584ab0c01a778fb022d77b5892c98a4fa5c95
pedro1191 <pedro_almeida1191@hotmail.com>
2019-03-27 11:14:02 -0300

    Update index and show methods and abstract api method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,31 @@ class Controller extends BaseController
 {
     use Helpers;
     const DEFAULT_PAGINATION_RESULTS = 15;
+
+    /**
+     * Call another API to fetch information.
+     * 
+     * @param string $url
+     * @return array
+     */
+    protected function requestToDashboardAPI(string $url)
+    {
+        // create curl resource
+        $ch = curl_init();
+
+        // set url
+        curl_setopt($ch, CURLOPT_URL, env('DASHBOARD_API_URL') . $url);
+
+        // return the transfer as a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        // $output contains the output string
+        $output = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        // close curl resource to free up system resources
+        curl_close($ch);
+
+        return ['output' => $output, 'httpCode' => $httpCode];
+    }
 }
--- app/Http/Controllers/V1/ProductController.php
+++ app/Http/Controllers/V1/ProductController.php
@@ -10,13 +10,14 @@ class ProductController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
     {
         $queryParams = str_replace($request->url(), '', $request->fullUrl());
 
-        $apiUrl = "/products$queryParams";
+        $apiUrl = '/products' . $queryParams;
 
         $response = $this->requestToDashboardAPI($apiUrl);
 
@@ -26,42 +27,18 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request, $id)
     {
-        $apiUrl = "/products/$id";
+        $queryParams = str_replace($request->url(), '', $request->fullUrl());
+
+        $apiUrl = '/products/' . $id . $queryParams;
 
         $response = $this->requestToDashboardAPI($apiUrl);
 
         return response()->json($response['output'], $response['httpCode']);
-    }
-
-    /**
-     * Call another API to fetch information about products.
-     * 
-     * @param string $url
-     * @return array
-     */
-    private function requestToDashboardAPI(string $url)
-    {
-        // create curl resource
-        $ch = curl_init();
-
-        // set url
-        curl_setopt($ch, CURLOPT_URL, env('DASHBOARD_API_URL') . $url);
-
-        // return the transfer as a string
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        // $output contains the output string
-        $output = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-        // close curl resource to free up system resources
-        curl_close($ch);
-
-        return ['output' => $output, 'httpCode' => $httpCode];
     }
 }
